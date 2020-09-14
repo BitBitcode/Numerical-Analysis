16ca3b6af9a66d3cf63d17e3587d3e49d7dbdb15
Acrylic-Studio <smilewwc@qq.com>
2020-09-15 00:01:19 +0800

    添加了通用函数，使得每次只需要修改通用函数中的调用即可（只修改一处），无需修改主函数中的调用（需要修改多处）

--- 二分法求方程根/二分法求方程根.c
+++ 二分法求方程根/二分法求方程根.c
@@ -1,12 +1,13 @@
 ﻿// 1、二分法求方程根
 
+
 #include<stdio.h>
 #include<math.h>
-
 #define PRECISION (0.001)	// 设置精度
 
-// 教材 P23 例2.1
-double f(double x)
+
+// 【数学函数】教材 P23 例2.1
+double function_1(double x)
 {
 	double y = 0;
 	y = x * x * x + 4 * x * x - 10;
@@ -14,13 +15,26 @@ double f(double x)
 }
 
 
+// 【数学函数】教材 P 例
+
+
+
+// 【通用函数】起到传递参数的作用，可以不用修改主函数中的代码，直接修改此处调用的数学表达式函数即可
+// 【注意】如果不声明，则只能定义在最后（否则本函数调用的其他函数会无法识别）
+double f(double x_value)
+{
+	return function_1(x_value);
+}
+
+
+// 【主函数】
 int main()
 {
 	float a = 0;		// 区间左端点
 	float b = 0;		// 区间右端点
 	int select = 0;		// 
 	float m = 0;		// 区间中点
-	float e_y = 0;		// y的绝对误差限
+	float e_y = 1;		// y的绝对误差限
 
 RE: // 循环标签
 	printf("请输入隔根区间 [a,b] 的端点值\n");
@@ -32,8 +46,8 @@ RE: // 循环标签
 	// 判别用户输入的区间是否有根
 	if (f(a) * f(b) < 0)
 	{
-		printf("将在 [%f,%f] 区间求解，当前精度设置为：%f\n", a, b, PRECISION);
-		printf("确认请按1，重新输入请按2\n");
+		printf("\n【核对】将在 [%f,%f] 区间求解，当前精度设置为：%f\n", a, b, PRECISION);
+		printf("确认请按 1，重新输入请按 2\n");
 		scanf_s("%d", &select);
 		if (select == 2)
 		{
@@ -53,22 +67,35 @@ RE: // 循环标签
 	// 二分法
 	do
 	{
-		m = (b - a) / 2 +a;
-		printf("中值：%f\n", m);
+		m = a + (b - a) / 2;		// 或者：m = (a + b) / 2
+		printf("m = %f\n", m);
+		printf("f(a) = %f\n", f(a));
+		printf("f(m) = %f\n", f(m));
+		printf("f(b) = %f\n", f(b));
+
 		if (f(a) * f(m) < 0)
 		{
+			
 			b = m;
+			printf("【1】\n");
 		}
-		else if (f(b) * f(m) < 0)
+		else if (f(m) * f(b) < 0)
 		{
 			a = m;
+			printf("【2】\n");
+		}
+		else if (f(m) == 0)
+		{
+			printf("【3】\n");
+			break;
 		}
 		else
 		{
 			printf("【错误】\n\n");
 		}
+		
 		e_y = abs(f(m) - 0);
-	} while (e_y > PRECISION);
+	} while (e_y > PRECISION); 
 
 	printf("\n【完成】\n满足设定精度的数值解为：x = %0.3f\n", m);
 	printf("误差为：f(x) - 0 = %f\n", e_y);
